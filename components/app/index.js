d93721ab3800463873e808819d4eb62278a31aa6
Binh Quan <binhqd@gmail.com>
2018-04-11 16:20:47 +0700

    Temporary disable authentication

--- components/app/index.js
+++ components/app/index.js
@@ -3,20 +3,20 @@ import {RenderRoutes} from 'base/routes';
 import { withRouter } from 'react-router';
 import {connect} from 'react-redux';
 import { Auth, User } from 'api';
-import {Categories} from 'api';
-import {isAdmin} from 'base/actions';
+import { Categories } from 'api';
+import { isAdmin } from 'base/actions';
 
 class App extends React.Component {
   constructor(props, context) {
     super(props, context);
 
-    this.props.dispatch(isAdmin()).then(response => {
-      if (response.isAuthenticated) {
-        // this.props.history.push(this.props.defaultPage);
-      } else {
-        this.props.history.push('/');
-      }
-    });
+    // this.props.dispatch(isAdmin()).then(response => {
+    //   if (response.isAuthenticated) {
+    //     // this.props.history.push(this.props.defaultPage);
+    //   } else {
+    //     this.props.history.push('/');
+    //   }
+    // });
 
     this.props.dispatch(Categories.actions.list({}, null, {}))
   }
